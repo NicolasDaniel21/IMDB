529438364b893f9c84f2bd24ef2b18d06553f578
NicolasDaniel13 <nicolasdanielw13@gmail.com>
2023-08-28 22:03:19 -0300

    Filme class added

---
+++ src/Filme.java
@@ -0,0 +1,13 @@
+public class Filme {
+    private String nome, diretor, tipo;
+    private int codigo, ano;
+    private String[] nomes;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+}
